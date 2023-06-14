38401a8615fd1194c9d2f03cfc60a995c700d594
javeria2108 <javeriazh@gmail.com>
2023-06-14 23:36:14 +0500

    initial commit

--- pages/success.js
+++ pages/success.js
@@ -25,10 +25,10 @@ export default function Success() {
     >
       <div className="bg-white rounded-lg w-1/3 font-latoRegular text-gray-700 p-16">
         <h1 className="text-3xl pb-4 font-latoBold">
-          Thanks for the email {router.query.name} ✨
+          Registration Successful
         </h1>
         <p className="text-lg  text-gray-500">
-          We have sent you an email over at {router.query.email}. We will get
+        We will get
           back to you as soon as we can!
         </p>
       </div>
